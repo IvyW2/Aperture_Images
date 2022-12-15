23194ac0a23f7e7f532d22dc2b45cae9c7ad3243
IvyW2 <ivywambugha2@gmail.com>
2022-12-15 19:15:35 +0300

    fix API URL- token acess

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-dayNightTheme = () => {
-    let date = new Date();
-    let hour = date.getHours();
-    
-    if(hour >= 7 && hour < 19){
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = 'black';
-    }
-    else{
-      document.body.style.backgroundColor = 'black';
-      document.body.style.color = 'white';
-    }
-   }
-    
-   window.addEventListener('load', dayNightTheme);
-    
-   document.querySelector("#input").addEventListener("keydown", (event) => {
+document.querySelector("#input").addEventListener("keydown", (event) => {
     if (event.key == "Enter")
       apiRequest();
    });
@@ -51,6 +35,6 @@ loadImages = (data) => {
       document.querySelector("#grid").appendChild(image);
     }
    }
-    
+   
    
    
